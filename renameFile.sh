c62c411444fe8450701aca74da1188343602977e
Elliott Hill <elliottjhill@gmail.com>
2013-11-10 17:11:16 +0000

    Added support for skipping .part and 10+ season series

--- renameFile.sh
+++ renameFile.sh
@@ -6,19 +6,24 @@ do
     echo "Skipping $f"
     continue
   fi
-  if [ $f == *.sh ] 
+  if [[ $f == *.sh ]]
+  then
+    echo "Not copying $f"
+    continue
+  fi
+  if [[ $f == *.part ]]
   then
     echo "Not copying $f"
     continue
   fi
   echo $f
-  name=$(echo $f | sed 's/\(.*\)\.S0[0-9].*/\1/')
+  name=$(echo $f | sed 's/\(.*\)\.S0\{0,1\}[0-9].*/\1/')
   name=$(echo $name | sed 's/\./ /g')
-  episodeAndSeason=$(echo $f | sed 's/.*\(S0[0-9]*E[0-9]*\).*/\1/')
-  season=$(echo $episodeAndSeason | sed 's/S0\([0-9]\)E[0-9]*/\1/')
+  episodeAndSeason=$(echo $f | sed 's/.*\(S0\{0,1\}[0-9]*E[0-9]*\).*/\1/')
+  season=$(echo $episodeAndSeason | sed 's/S0\{0,1\}\([0-9]*\)E[0-9]*/\1/')
   episode=$(echo $episodeAndSeason | sed 's/S[0-9]*E\([0-9]*\)/\1/')
   format=$(echo $f| sed 's/.*\.\(.*\)/\1/')
-  #echo "$name $episodeAndSeason $season $episode"
+#  echo "$name $episodeAndSeason $season $episode"
   episodeAndSeason="$season x $episode"
   episodeAndSeason=$(echo $episodeAndSeason | sed 's/ //g')
   path="$name/Season $season/$name $episodeAndSeason.$format"
@@ -34,6 +39,6 @@ do
     echo "Creating $seasonPath"
     mkdir "$name/Season $season"
   fi
-  mv -f $f "$path" 
+  mv -f $f "$path"
 
 done
